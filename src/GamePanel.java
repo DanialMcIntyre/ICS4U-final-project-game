955937fb659d4c175edef2f2004621fbfcd5fce0
Danie <31494242+DanialMcIntyre@users.noreply.github.com>
2022-06-02 10:54:08 -0400

    Combined collision classes

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -23,8 +23,8 @@ public class GamePanel extends JPanel implements ActionListener {
     //Objects
     Kart kart;
 
-    CollidableRectangleObstacle ro1 = new CollidableRectangleObstacle(200, 500, 1500, 100, Color.BLACK, true);
-    UncollidableRectangleObstacle uro1 = new UncollidableRectangleObstacle(1000, 250, 200, 600, Color.DARK_GRAY, true);
+    RectangleObstacle ro1 = new RectangleObstacle(200, 500, 1500, 100, Color.BLACK, true);
+    RectangleObstacle ro2 = new RectangleObstacle(1000, 250, 200, 600, Color.DARK_GRAY, true);
     CircleObstacle co1 = new CircleObstacle(100, 200, 50, 100, Color.BLUE, true);
    
     public GamePanel() {
@@ -59,8 +59,8 @@ public class GamePanel extends JPanel implements ActionListener {
         g.drawString("Speed: " + String.valueOf(Math.round((kart.getAcceleration() * kart.getAccTime()) / kart.getTractionLevel())), 500, 500);
 
         //Draws obstacles
-        uro1.draw(g);
         ro1.draw(g);
+        ro2.draw(g);
         co1.draw(g);
 
         //Draws kart
@@ -75,14 +75,32 @@ public class GamePanel extends JPanel implements ActionListener {
  
         //Checks collision for obstacles
         ro1.collision(kart);
-        uro1.collision(kart);
+        ro2.collision(kart);
+
+        onCollision();
 
         //Updates kart position
         kart.updateKart();
 
+
         //Repaints screen
         repaint();
  
+    }
+
+    //Does stuff when collision occur
+    public void onCollision() {
+
+        if (ro1.getIsCollided() && !(kart.getIsDeccelerating())) {
+            kart.setAccTime(0);
+        }
+
+        if (ro2.getIsCollided()) {
+            kart.setTractionLevel(2);
+        } else {
+            kart.setTractionLevel(1);
+        }
+
     }
  
 }
--- src/obstacles/RectangleObstacle.java
+++ src/obstacles/RectangleObstacle.java
@@ -4,10 +4,16 @@ import java.awt.*;
 
 import src.*;
 
-public class UncollidableRectangleObstacle extends Obstacle {
+public class RectangleObstacle extends Obstacle {
 
-    public UncollidableRectangleObstacle(int x, int y, int w, int h, Color c, boolean t) {
+    private boolean isCollided = false;
+
+    public RectangleObstacle(int x, int y, int w, int h, Color c, boolean t) {
         super(x, y, w, h, c, t);
+    }
+
+    public boolean getIsCollided() {
+        return this.isCollided;
     }
 
     public void draw(Graphics g) {
@@ -16,15 +22,18 @@ public class UncollidableRectangleObstacle extends Obstacle {
     }
 
     public void collision(Kart k) {
-        k.setTractionLevel(1);
+
+        if (!(k.getIsDeccelerating())) {
+            this.isCollided = false;
             for (int i = k.getXPos(); i < k.getXPos() + k.getWidth(); i++) {
                 if (i > this.getXPos() && i < this.getXPos() + this.getWidth()) {
                     for (int n = k.getYPos(); n < k.getYPos() + k.getHeight(); n ++) {
                         if (n > this.getYPos() && n < this.getYPos() + this.getHeight()) {
-                            k.setTractionLevel(2);
-                        }
+                            this.isCollided = true;
+                        } 
                     }
-                }
+                } 
             }
+        } 
     }
 }
