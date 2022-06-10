fde24330adc9bac2ef3bd06738b46b322002b9a2
Stephen Rebel <75142837+StephenRebel@users.noreply.github.com>
2022-06-10 09:22:46 -0400

    Finished up the Play Menu

--- src/gameWindows/PlayMenu.java
+++ src/gameWindows/PlayMenu.java
@@ -5,7 +5,7 @@ import java.awt.image.BufferedImage;
 
 public class PlayMenu extends GeneralWindow {
 
-    private int kartNum;
+    private int kartNum = 6;
     
     public void drawPlayMenu(Graphics g, BufferedImage[] karts) {
 
@@ -33,9 +33,17 @@ public class PlayMenu extends GeneralWindow {
         g.drawString("Choose Your Kart", 190, 180);
 
         //Probably vould use for loop but my brian is too fryed
-        g.drawImage(karts[0], (int)((760*.25) - (karts[0].getWidth()*.5) + 150), (int)((800/3) - (karts[0].getHeight()*.5) + 150), (int)(karts[0].getWidth()*.5), (int)(karts[0].getHeight()*.5), null);
-        g.drawImage(karts[1], (int)((760*.5) - (karts[1].getWidth()*.5) + 175), (int)((800/3) - (karts[1].getHeight()*.5) + 150), (int)(karts[1].getWidth()*.5), (int)(karts[1].getHeight()*.5), null);
-        g.drawImage(karts[2], (int)((760*.75) - (karts[2].getWidth()*.5) + 200), (int)((800/3) - (karts[1].getHeight()*.5) + 150), (int)(karts[2].getWidth()*.5), (int)(karts[2].getHeight()*.5), null);
+        g.drawImage(karts[0], (int)((760*.25) - (karts[0].getWidth()*.5) + 150), (int)((800*.25) - (karts[0].getHeight()*.5) + 225), (int)(karts[0].getWidth()*.5), (int)(karts[0].getHeight()*.5), null);
+        g.drawImage(karts[1], (int)((760*.5) - (karts[1].getWidth()*.5) + 175), (int)((800*.25) - (karts[1].getHeight()*.5) + 225), (int)(karts[1].getWidth()*.5), (int)(karts[1].getHeight()*.5), null);
+        g.drawImage(karts[2], (int)((760*.75) - (karts[2].getWidth()*.5) + 200), (int)((800*.25) - (karts[2].getHeight()*.5) + 225), (int)(karts[2].getWidth()*.5), (int)(karts[2].getHeight()*.5), null);
+        g.drawImage(karts[3], (int)((760*.25) - (karts[3].getWidth()*.5) + 150), (int)((800*.5) - (karts[3].getHeight()*.5) + 225), (int)(karts[3].getWidth()*.5), (int)(karts[3].getHeight()*.5), null);
+        g.drawImage(karts[4], (int)((760*.5) - (karts[4].getWidth()*.5) + 175), (int)((800*.5) - (karts[4].getHeight()*.5) + 225), (int)(karts[4].getWidth()*.5), (int)(karts[4].getHeight()*.5), null);
+        g.drawImage(karts[5], (int)((760*.75) - (karts[5].getWidth()*.5) + 200), (int)((800*.5) - (karts[5].getHeight()*.5) + 225), (int)(karts[5].getWidth()*.5), (int)(karts[5].getHeight()*.5), null);
+        g.drawImage(karts[6], (int)((760*.25) - (karts[6].getWidth()*.5) + 150), (int)((800*.75) - (karts[6].getHeight()*.5) + 225), (int)(karts[6].getWidth()*.5), (int)(karts[6].getHeight()*.5), null);
+        g.drawImage(karts[7], (int)((760*.5) - (karts[7].getWidth()*.5) + 175), (int)((800*.75) - (karts[7].getHeight()*.5) + 225), (int)(karts[7].getWidth()*.5), (int)(karts[7].getHeight()*.5), null);
+        g.drawImage(karts[8], (int)((760*.75) - (karts[8].getWidth()*.5) + 200), (int)((800*.75) - (karts[8].getHeight()*.5) + 225), (int)(karts[8].getWidth()*.5), (int)(karts[8].getHeight()*.5), null);
+
+        g.drawImage(karts[kartNum], (int)(1440 - (karts[kartNum].getWidth()*.75)), (int)(500 - (karts[kartNum].getHeight()*1.25)), (int)(karts[kartNum].getWidth()*1.5), (int)(karts[kartNum].getHeight()*1.5), null);
 
         g2.setStroke(oldStroke);
 
